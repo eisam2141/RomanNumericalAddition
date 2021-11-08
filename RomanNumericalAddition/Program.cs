221acc6f95d78258c04197741e912c0b8a8eee73
Eisa Mustafa <eisam2141@gmail.com>
2021-11-07 23:22:59 -0500

    Initial project

---
+++ RomanNumericalAddition/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RomanNumericalAddition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Console.ReadLine();
+            List<int> lst = new List<int>();
+            foreach(string i in a.Split(' '))
+            {
+                var b = DataGen.RomanNumerals.RomanNumeralsExtensions.ParseRomans(i.ToUpper());
+                lst.Add(b);
+            }
+            var k = lst.Sum();
+
+            var output = DataGen.RomanNumerals.RomanNumeralsExtensions.ToRomans(k);
+
+            Console.WriteLine($"Total of {a} = {output}")
+
+           
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
